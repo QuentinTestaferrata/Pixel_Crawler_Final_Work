c99793da9ea7c96ac28f5c48d8063310a30bd062
QuentinTestaferrata <133092199+QuentinTestaferrata@users.noreply.github.com>
2025-02-05 16:15:34 +0100

    Feature: Bone Shield
    New defensive attack for Bone Staff

--- Entities/Weapons/Staffs/Bone Staff/bone_staff.gd
+++ Entities/Weapons/Staffs/Bone Staff/bone_staff.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
-const BONE_SHARD = preload("res://Entities/Weapons/Staffs/Bone Staff/attacks/bone_shard.tscn")
+const BONE_SHARD = preload("res://Entities/Weapons/Staffs/Bone Staff/attacks/Primary/bone_shard.tscn")
+const BONE_SHIELD = preload("res://Entities/Weapons/Staffs/Bone Staff/attacks/Secondary/bone_shield.tscn")
 
 var primary: PackedScene
 var secondary: PackedScene
 
 @export var primary_attack: PROJECTILE
+@export var secondary_attack: PROJECTILE
+@export var bone_amount: int = 2
 
 @onready var primary_attack_timer: Timer = $PrimaryAttackTimer
 @onready var marker: Marker2D = $Marker2D
@@ -19,7 +22,7 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_released("primary_attack"):
 		primary_attack_timer.stop()
 	if event.is_action_pressed("secondary_attack"):
-		pass
+		shoot_secondary()
 
 func shoot_primary() -> void:
 	var bone_shard = BONE_SHARD.instantiate()
@@ -34,3 +37,11 @@ func shoot_primary() -> void:
 
 func _on_primary_attack_timer_timeout() -> void:
 	shoot_primary()
+
+func shoot_secondary() -> void:
+	var player = get_tree().get_first_node_in_group("player")
+	var time_to_wait: float = 1.5 / bone_amount 
+	for i in bone_amount:
+		var temp_bone_shield = BONE_SHIELD.instantiate()
+		player.add_child(temp_bone_shield)
+		await get_tree().create_timer(time_to_wait).timeout 
