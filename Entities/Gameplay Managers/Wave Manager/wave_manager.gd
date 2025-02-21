07e0232a36be2d4ed05dd1b36b1b5c8149fe7852
QuentinTestaferrata <133092199+QuentinTestaferrata@users.noreply.github.com>
2025-02-21 17:57:40 +0100

    Feature: new zombie enemies
    4 new zombie enemies added to the game, with new weapons. still have to make some new animations for the weapons.

--- Entities/Gameplay Managers/Wave Manager/wave_manager.gd
+++ Entities/Gameplay Managers/Wave Manager/wave_manager.gd
@@ -52,7 +52,7 @@ func _ready() -> void:
 	current_wave = 1
 	total_kills = 0
 	spawned_enemies = 0
-	#spawn_interval_timer.start()
+	spawn_interval_timer.start()
 	max_enemies_alive = (max_percentage_enemies_alive / 100) * enemies_per_wave
 
 func _on_spawn_interval_timeout() -> void:
@@ -90,7 +90,6 @@ func _on_spawn_interval_timeout() -> void:
 		spawned_enemies += 1
 	else: 
 		spawn_interval_timer.start()
-
 		
 	if spawned_enemies >= enemies_per_wave:
 		spawn_interval_timer.stop()
