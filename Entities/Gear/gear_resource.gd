71ec6c89a1c03faf36aa168528c32d4e5f355dc8
LucasDeLil <104128338+LucasDeLil@users.noreply.github.com>
2025-02-26 13:41:25 +0100

    Gear resource created + Remake zone 3
    Zone 3 has a problem like zone 2 where everything needed to be moved for the player to appear infront of the tree logs but behind the leafs. Still needs fixes but is already partially fixed.

---
+++ Entities/Gear/gear_resource.gd
@@ -0,0 +1,28 @@
+extends Resource
+class_name GearData
+
+enum gear_types {HAT, COAT, BOOTS, RING, AMULETTE}
+enum rarity {COMMON, UNCOMMON, RARE, EPIC, LEGENDARY, MYTHICAL}
+
+@export_category("Gear Data")
+@export var gear_name: String
+@export var scene: PackedScene
+@export var sprite: Texture
+@export var shop_sprite: Texture
+@export var price: int
+@export var obtained: bool = false
+@export var equip_slot: int = 0
+
+@export_category("Gear Stats")
+@export var gear_type: gear_types
+@export var gear_rarity: rarity
+@export var damage: int
+@export var defense: int
+@export var speed: int
+@export var extra_health: int
+@export var regenaration: int
+@export var fire_rate: int
+@export var critical_hit: int
+
+@export_category("Other")
+@export_multiline var description: String
